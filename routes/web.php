210e147034b8addb87dd03f96eb8e5813a48af44
Reydin <reydinyaswi@gmail.com>
2023-02-25 12:01:51 +0700

    Update routes web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PekerjaController;
 use App\Http\Controllers\CplDiktiController;
 
 /*
@@ -18,16 +17,6 @@ use App\Http\Controllers\CplDiktiController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/pekerja',[PekerjaController::class, 'index'])->name('pekerja');
-
-Route::get('/tambahpekerja',[PekerjaController::class, 'tambahpekerja'])->name('tambahpekerja');
-Route::post('/insertpekerja',[PekerjaController::class, 'insertpekerja'])->name('insertpekerja');
-
-Route::get('/tampilpekerja/{id}',[PekerjaController::class, 'tampilpekerja'])->name('tampilpekerja');
-Route::post('/updatepekerja/{id}',[PekerjaController::class, 'updatepekerja'])->name('updatepekerja');
-
-Route::get('/delete/{id}',[PekerjaController::class, 'delete'])->name('delete');
 
 Route::get('/cpldikti',[CplDIktiController::class, 'indexDikti'])->name('cpldikti');
 
